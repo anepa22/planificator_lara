d987d7846f6987285c630c676bfd0afbd8003063
Anepa <anepa22@gmail.com>
2026-08-20 17:32:34 -0300

    Correccion

--- frontend/planificador/src/components/AppMenu.jsx
+++ frontend/planificador/src/components/AppMenu.jsx
@@ -66,7 +66,7 @@ export default function AppMenu({
       >
         <div className="app-menu-head">
           <div>
-            <div className="app-menu-title">Planificador Gisela*</div>
+            <div className="app-menu-title">Planificador Gisela</div>
             {userLabel && <div className="app-menu-user">{userLabel}</div>}
             <div className="app-menu-version">v{version}</div>
           </div>
